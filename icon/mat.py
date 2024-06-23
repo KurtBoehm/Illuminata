4d780d83f3c07b8b1b225fe657882d0b039ac60e
Kurt Böhm <kurbo96@gmail.com>
2024-06-23 13:34:07 +0200

    Update icon

--- icon/mat.py
+++ icon/mat.py
@@ -1,6 +1,11 @@
 import numpy as np
 
 
+def mat(msg: str):
+    l = [float(v) for v in msg.split()]
+    return np.array([l[::2], l[1::2]])
+
+
 def rotate_mat(angle: float):
     theta = np.radians(angle)
     c, s = np.cos(theta), np.sin(theta)
